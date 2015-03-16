6de33fef466af593211bf72f9289f527dcff23cf
a.franceschini <a.franceschini@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-03-16 13:32:46 +0000

    bug fix on tests
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/STRINGdb@100715 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_get_homologs_besthits.R
+++ inst/unitTests/test_get_homologs_besthits.R
@@ -1,5 +1,5 @@
 test_get_homologs_besthits <- function() {
-  string_db <- STRINGdb$new( version="9_05", species=9606, score_threshold=400 ) 
+  string_db <- STRINGdb$new( version="10", species=9606, score_threshold=400 ) 
   data(diff_exp_example1)
   example1_mapped = string_db$map( diff_exp_example1, "gene", removeUnmappedRows = TRUE )  
   homologs = string_db$get_homologs_besthits( subset(example1_mapped, gene=="TP53")$STRING_id )
